9739b76dbf0b873df0521d1ca6e4f36008f89a16
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-22 21:55:04 -0400

    This is good enough

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
+const graphql = require('express-graphql')
+// required to read files and schema:
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 const port = process.env.port || 3000
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
+// building a schema from another file
+const schemaCode = fs.readFileSync(path.join(__dirname, 'lib', 'schema.graphql'), 'utf8')
 
+const messages = [
+  {
+    id: '1',
+    text: 'This is a message',
+    author: 'F Thot Fitzgerald',
+    comment: 'Whatever'
+  },
+  {
+    id: '2',
+    text: 'This is another message',
+    author: 'Hulk Hogan',
+    comment: 'Brother'
+  },
+  {
+    id: '3',
+    text: 'This is yet a third message',
+    author: 'Steve Tide Pods',
+    comment: 'Whatever'
+  }
+]
+
+// Define schema
+const schema = buildSchema(schemaCode)
+
+// Create resolvers
 const root = {
-  hello: () => {
-    return 'Hello World'
+  getAllMessages: () => {
+    return messages
+  },
+  getMessage: id => {
+    const message = messages.find(message => message.id === id)
+    if (!message) {
+      throw new Error('This message does not exist')
+    }
+    else {
+      return messages.find(message => message.id === id)
+    }
   }
 }
 
-app.listen(port, () => {
-  console.log(`Server started http://localhost:${port}`)
-})
+// Put graphql in the end point
+app.use('/api', graphql({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
 
-graphql(schema, '{ hello }', root).then(data => {
-  console.log(data)
+app.listen(port, () => {
+  console.log(`Server started http://localhost:${port}/api`)
 })
